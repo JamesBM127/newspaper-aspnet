50b798ca00b44fc8b158aa9b7b0c2ad4e2a3e803
Jameson B Martins <57304746+JamesBM127@users.noreply.github.com>
2021-06-04 01:03:14 -0400

    Context DB Sqlserve

---
+++ Newspaper/Data/NewspaperContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Newspaper.Data
+{
+    public class NewspaperContext : DbContext
+    {
+        public NewspaperContext(DbContextOptions<NewspaperContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Newspaper/Models/ViewModel/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Newspaper.Models.ViewModel
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
